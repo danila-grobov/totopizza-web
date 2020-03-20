795bab9a5b0ba755273d6683a679a1620591dc89
Danila Grobov <danila.grob@gmail.com>
2020-03-20 22:07:17 +0200

    Use nodemon to apply changes without restarting the server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const NodemonPlugin = require('nodemon-webpack-plugin');
 
 module.exports = {
   entry: './dev/app.js',
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, ''),
+    path: path.resolve(__dirname, 'prod'),
   },
+  plugins: [
+      new NodemonPlugin(),
+  ],
+  target: "node",
+  devtool: "source-map"
 };
