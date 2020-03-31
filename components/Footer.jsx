ddeaf7d0ab5528f6dae06d2e41f8e4aa73708ae8
Danila Grobov <danila.grob@gmail.com>
2020-03-31 23:29:36 +0300

    Create Contacts component.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <div className="Footer">
-
+            <div className="footer">
+                
             </div>
         );
     }
