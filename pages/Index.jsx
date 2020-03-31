ea0fb5822a04055e3c35fa645be97324626d372b
Danila Grobov <danila.grob@gmail.com>
2020-04-01 00:59:05 +0300

    Create order page.

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -9,7 +9,7 @@ export default class Index extends React.Component {
             <div className="app">
                 <Header />
                 <div className="app__backgroudImage">
-                    <div className="app__whiteBlob">
+                    <div className="app__blob app__blob--white">
                         <img src="images/logo.png" alt="Toto Pizza" className="app__logoImage"/>
                         <PizzaList />
                         <PizzaRolls />
---
+++ pages/uzsakymas.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+class uzsakymas extends Component {
+    render() {
+        return (
+            <div className="app">
+                <Header />
+                <div className="app__backgroudImage app__backgroudImage--orderPage">
+                    <div className="app__blob app__blob--granite">
+                        
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default uzsakymas;
