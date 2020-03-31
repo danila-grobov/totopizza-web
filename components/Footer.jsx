4b849009b907f1efd18edffdede90fea864a4dd6
Danila Grobov <danila.grob@gmail.com>
2020-03-31 23:56:46 +0300

    Create and style orderButton.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,7 +9,13 @@ class Footer extends Component {
                     <Contacts icon="images/place.svg" text="Tilžės g. 5, Šilutė 99142"/>
                     <Contacts icon="images/email.svg" text="plarpalas@gmail.com"/>
                 </div>
-                
+                <div className="orderButton">
+                    <span className="orderButton__text">
+                        UŽSISAKYK
+                    </span>
+                </div>
+                <img src="images/fb.svg" className="socialButton"/>
+                <img src="images/insta.svg" className="socialButton"/>
             </div>
         );
     }
