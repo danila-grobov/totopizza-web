1500d111a4642e64cd6a70d7b0aac6420a6a1251
Danila Grobov <danila.grob@gmail.com>
2020-03-31 23:39:53 +0300

    Make use of Contacts component.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import Contacts from './Contacts';
 class Footer extends Component {
     render() {
         return (
             <div className="footer">
+                <div className="footer__contacts">
+                    <Contacts icon="images/phone.svg" text="+370 639 99399"/>
+                    <Contacts icon="images/place.svg" text="Tilžės g. 5, Šilutė 99142"/>
+                    <Contacts icon="images/email.svg" text="plarpalas@gmail.com"/>
+                </div>
                 
             </div>
         );
