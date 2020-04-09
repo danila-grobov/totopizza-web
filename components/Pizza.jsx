6863973946c3002c2ee4766b3958f96b7cfd7d0c
Danila Grobov <danila.grob@gmail.com>
2020-04-09 18:52:59 +0300

    Add currency symbol

--- components/Pizza.jsx
+++ components/Pizza.jsx
@@ -2,29 +2,29 @@ import React from 'react';
 
 export default class App extends React.Component {
     render() {
-        const {src, title, ingredients, expensive} = this.props;
+        const { src, title, ingredients, expensive } = this.props;
         const expensive_large = 7.49;
         const expensive_small = 5.49;
         const cheap_large = 6.89;
         const cheap_small = 4.89;
+        const currency = "€";
         return (
-            <div className="pizza"> 
+            <div className="pizza">
                 <div className="pizza__imageBlob">
-                    <img className="pizza__image" src={src}/>
-                </div>   
+                    <img className="pizza__image" src={src} />
+                </div>
                 <div className="pizza__info">
                     <span className="pizza__infoTitle">{title}</span>
                     <span className="pizza__infoIngredients">{ingredients}</span>
                 </div>
                 <div className="pizza__small">
                     <span className="pizza__smallSize">32cm</span>
-                    
-                    <span className="pizza__smallPrice">{expensive ? expensive_small : cheap_small}</span>
+                    <span className="pizza__smallPrice">{expensive ? expensive_small : cheap_small}{currency}</span>
                 </div>
-                <div className="pizza__spacer"/>
+                <div className="pizza__spacer" />
                 <div className="pizza__large">
                     <span className="pizza__largeSize">42cm</span>
-                    <span className="pizza__largePrice">{expensive ? expensive_large : cheap_large}</span>
+                    <span className="pizza__largePrice">{expensive ? expensive_large : cheap_large}{currency}</span>
                 </div>
             </div>
         );
