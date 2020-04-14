a8521a1e542dd0a29242993ba2018d5da5ae0b80
Danila Grobov <danila.grob@gmail.com>
2020-04-14 22:46:40 +0300

    Add a header for expandable menu.

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,11 +3,15 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-                    <div className="header__wrapper">
-                        <img src="images/toto_logo.png" className="header__logoButton"/>
-                        <a href="/" className="header__link">Pagrindinis</a>
-                        <a href="/" className="header__link header__link--highlighted">Užsisakyk</a>
-                    </div>
+                <div className="header__wrapper">
+                    <img src="images/toto_logo.png" className="header__logoButton" />
+                    <a href="/" className="header__link">Pagrindinis</a>
+                    <a href="/" className="header__link header__link--highlighted">Užsisakyk</a>
+                </div>
+                <div className="menuHeader">
+                    <span className="menuHeader__title">Menu</span>
+                    <img src="images/close.svg" className="menuHeader__close" />
+                </div>
             </div>
         );
     }
