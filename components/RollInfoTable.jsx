3057bfe07a648f6b74918d82bded21120e586814
Danila Grobov <danila.grob@gmail.com>
2020-04-23 00:14:31 +0300

    Add roll with chicken.

--- components/RollInfoTable.jsx
+++ components/RollInfoTable.jsx
@@ -7,25 +7,34 @@ class RollInfoTable extends Component {
                 <div className="RollInfoTable__hWrapper">
                     <RollInfo
                         title="SU KUMPIU"
-                        content="pomidorų padažas, sūris, kiaulienos kumpis, pievagrybiai, konservuoti kukurūzai" 
-                        vertical={true}/>
+                        content="pomidorų padažas, sūris, kiaulienos kumpis, pievagrybiai, konservuoti kukurūzai"
+                        vertical={true} />
                     <div className="RollInfoTable__vSpacer" />
                     <RollInfo
                         title="SU MALTA KIAULIENA"
                         content="pomidorų padažas, sūris, malta kiauliena, jelapenas, BBQ padažas"
-                        vertical={true}/>
+                        vertical={true} />
                     <div className="RollInfoTable__vSpacer" />
                     <RollInfo
                         title="SU JAUTIENA"
                         content="pomidorų padažas, sūris, rūkyta jautiena,
-                        pievagrybiai, švž. paprika" 
-                        vertical={true}/>
+                        pievagrybiai, švž. paprika"
+                        vertical={true} />
+                    <div className="RollInfoTable__vSpacer" />
+
                 </div>
                 <div className="RollInfoTable__hSpacer" />
-                <RollInfo
-                    title="VEGETARISKAS"
-                    content="pomidorų padažas, sūris, pievagrybiai, konservuoti kukurūzai, svogūnai, švž.paprika" 
-                    vertical={false}/>
+                <div className="RollInfoTable__hWrapper RollInfoTable__hWrapper--3c">
+                    <RollInfo
+                        title="VEGETARISKAS"
+                        content="pomidorų padažas, sūris, pievagrybiai, konservuoti kukurūzai, svogūnai, švž.paprika"
+                        vertical={true} />
+                    <div className="RollInfoTable__vSpacer" />  
+                    <RollInfo
+                        title="SU VIŠTIENA"
+                        content="pomidorų padažas, sūris, vištiena, pievagrybiai, švž. paprika"
+                        vertical={true} />
+                </div>
             </div>
         );
     }
