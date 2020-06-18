20114494178282786adc308cb20cc9db845c1ef5
EatACarrot <mantukiss456@gmail.com>
2020-06-18 20:22:14 +0100

    messed up the price, added a naujina bool

--- components/Pizza.jsx
+++ components/Pizza.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 export default class App extends React.Component {
     render() {
-        const { src, title, ingredients, expensive, ignoreSmallPrice, spiceLvl, vegie } = this.props;
+        const { src, title, ingredients, expensive, ignoreSmallPrice, spiceLvl, vegie , naujiena} = this.props;
+        const expensive_large_new = 7.99;
+        const expensive_small_new = 5.99;
         const expensive_large = 7.49;
         const expensive_small = 5.49;
         const cheap_large = 6.89;
         const cheap_small = 4.89;
         const notThere = "N\\A";
         const currency = "€";
-        const priceSmall = (expensive ? expensive_small : cheap_small) + currency;
+        const priceSmall = (expensive ? expensive_small : cheap_small);
+        const priceLarge = expensive ? expensive_large : cheap_large;
+        const finalPriceSmall = (naujiena ? expensive_small_new : priceSmall) + currency;
         let modifiers = [];
+        let imageMods = [];
         if (spiceLvl == 1) {
             modifiers = [ ...modifiers,
                 <img src="images/hot.svg" alt="aštru" className="pizza__modifierIcon" />,
@@ -28,10 +33,18 @@ export default class App extends React.Component {
                 <img src="images/vegie.svg" alt="vege" className="pizza__modifierIcon" />
             ]
         }
+        if(naujiena){
+            imageMods = [ ...imageMods,
+                <img src="images/naujiena.png" alt="naujiena" className="pizza__imageModIcon" />
+            ]
+        }
 
         return (
             <div className="pizza">
                 <div className="pizza__imageBlob">
+                    <div className="pizza__imageModifiers">
+                        {imageMods}
+                    </div>
                     <img className="pizza__image" src={src} />
                 </div>
                 <div className="pizza__info">
@@ -44,12 +57,12 @@ export default class App extends React.Component {
                 <div className="pizza__orderDetails">
                     <div className="pizza__small">
                         <span className="pizza__smallSize">32cm</span>
-                        <span className="pizza__smallPrice">{ignoreSmallPrice ? notThere : priceSmall}</span>
+                        <span className="pizza__smallPrice">{ignoreSmallPrice ? notThere : finalPriceSmall}</span>
                     </div>
                     <div className="pizza__spacer" />
                     <div className="pizza__large">
                         <span className="pizza__largeSize">42cm</span>
-                        <span className="pizza__largePrice">{expensive ? expensive_large : cheap_large}{currency}</span>
+                        <span className="pizza__largePrice">{naujiena ? expensive_large_new : priceLarge}{currency}</span>
                     </div>
                 </div>
             </div>
