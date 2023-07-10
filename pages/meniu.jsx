ab0832501a63cc6b45c597e5f797f7c60a15fe63
EatACarrot <mantukiss456@gmail.com>
2023-07-10 11:31:43 +0100

    meniu changes

--- pages/meniu.jsx
+++ pages/meniu.jsx
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
 import MetaTags from 'react-meta-tags';
 
+
 class meniu extends Component {
     componentDidMount() {
       // Simulating file download
       const fileName = 'TOTO_MENIU.pdf';
   
       fetch(fileName)
-        .then(response => response.blob())
-        .then(blob => {
+      .then(response => response.blob())
+      .then(blob => {
+        // Check if the browser supports the "msSaveBlob" method
+        if (window.navigator && window.navigator.msSaveBlob) {
+          window.navigator.msSaveBlob(blob, fileName);
+          window.location.href = '/';
+        } else {
           const url = URL.createObjectURL(blob);
           const link = document.createElement('a');
           link.href = url;
           link.setAttribute('download', fileName);
           link.click();
           URL.revokeObjectURL(url);
-        })
+          window.location.href = '/';
+        }
+      })
         .catch(error => console.log('Error:', error));
-        window.location.href = '/';
     }
   
     render() {
